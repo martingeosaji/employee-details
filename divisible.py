43209a5fd4fb65695757ab0f2e0c72a209a02a6d
martingeosaji <martingeo97@gmail.com>
2020-12-10 22:38:54 +0530

    divisible

---
+++ divisible.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number"))
+if(a%8)==0:
+    print("it is divisbile" )
+else:
+    print("not divisible")
---
+++ even.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number"))
+if(a%2)==0:
+    print("its even" )
+else:
+    print("its odd")
